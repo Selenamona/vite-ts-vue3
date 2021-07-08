e1fc60db49eb99e48f6cba6fdb2de3e824677e6b
selenamona <1591541371@qq.com>
2021-07-08 14:03:20 +0800

    tab切换

--- src/components/SwitchTab.vue
+++ src/components/SwitchTab.vue
@@ -1,16 +1,35 @@
 
 <!-- 页签切换 -->
 <template>
-  <div class="SwitchTab"></div>
+  <div class="SwitchTab">
+    <div
+      class="item"
+      v-for="item in tabList"
+      :key="item.key"
+      @click="onChange(item.key)"
+    >{{ item.name }}</div>
+  </div>
 </template>
 
 <script lang='ts'>
+import { SetupContext } from 'vue';
 
 export default {
   name: 'SwitchTab',
-  setup(props) {
+  props: {
+    tabList: {
+      type: Array,
+      default: []
+    }
+  },
+  setup(props: any, { emit }: SetupContext) {
+    const tabList = props.tabList
+    const onChange = (key: string) => {
+      emit("tabChange", key)
+    }
     return {
-
+      onChange,
+      tabList
     };
   },
   components: {}
--- src/views/list/List.vue
+++ src/views/list/List.vue
@@ -1,18 +1,31 @@
 <!-- 列表页 -->
 <template>
-  <div class="List"></div>
+  <div class="List">
+    <SwitchTabVue :tab-list="tabList" @tabChange="tabChange" />
+    <section v-show="activeKey === 'hot'">选择了热门车型</section>
+    <section v-show="activeKey === 'installment'">选择了分期购车</section>
+  </div>
 </template>
 
 <script lang='ts'>
+import SwitchTabVue from "@/components/SwitchTab.vue";
+import { reactive, ref } from "@vue/reactivity";
 
 export default {
   name: 'List',
-  setup(props) {
+  setup(props: any) {
+    let activeKey = ref('hot')
+    const tabList = ref([{ name: "热门", key: "hot" }, { name: "分期购车", key: "installment" }])
+    const tabChange = (key: string) => {
+      activeKey.value = key
+    }
     return {
-
+      activeKey,
+      tabList,
+      tabChange
     };
   },
-  components: {}
+  components: { SwitchTabVue }
 };
 </script>
 <style lang='scss' scoped>
