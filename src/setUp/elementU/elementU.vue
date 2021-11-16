1937e15f174a225832214d18ec260be8a35cb8ea
madongxue <madongxue@yxqiche.com>
2021-11-16 15:56:49 +0800

    add

--- src/setUp/elementU/elementU.vue
+++ src/setUp/elementU/elementU.vue
@@ -2,13 +2,19 @@
 import TableTreeSelectVue from "./table-tree-select.vue";
 import TableMergeVue from "./table-merge.vue";
 import TreeVue from "./Tree.vue";
+import TableMergeCheckVue from "./Table-merge-check.vue";
 </script>
 
 <template>
   <!-- 表格-树结构-多选 -->
   <!-- <TableTreeSelectVue /> -->
+
   <!-- 表格-合并行 -->
   <!-- <TableMergeVue /> -->
+
+  <!-- 表格-合并行-级联多选 -->
+  <TableMergeCheckVue />
+
   <!-- 树-多选-搜索 -->
-  <TreeVue />
+  <!-- <TreeVue /> -->
 </template>
